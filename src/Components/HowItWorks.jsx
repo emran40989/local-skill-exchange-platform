83d58bda25827bb1fd7de0d26793311a70714233
Emran Hossen <emran40989@gmail.com>
2025-11-25 05:21:23 +0600

    top rated updated

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import i1 from '../assets/emran.jpg'
+import i1 from "../assets/emran.jpg";
 
 const HowItWorks = () => {
   return (
@@ -8,45 +8,41 @@ const HowItWorks = () => {
         <div className="">
           <div className="py-8 bg-base-200 rounded-lg">
             <h2 className="text-3xl text-center font-bold my-4">
-              Top Rated Providers
+              How It Works
             </h2>
-          
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 container mx-auto">
-            <div className="bg-base-200 shadow-sm flex flex-col items-center justify-center min-h-[300px] rounded-lg">
-              <figure className="h-[120px] w-[120px] rounded-full overflow-hidden">
-                <img src={i1} alt="image" className="rounded-full" />
-              </figure>
-              <h2 className="pt-4 text-2xl font-bold">Emran Hossen</h2>
-              <h3 className="text-xl text-blue-400 font-semibold">Spoken English Practice</h3>
-              <div className="flex gap-2 items-center py-2">
-                <p className="font-bold text-xl">Rating: </p>
-                <span>⭐⭐⭐⭐⭐</span>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 container mx-auto">
+              <div className="bg-base-200 shadow-sm flex flex-col items-center justify-center min-h-[300px] rounded-lg">
+                <figure className="h-[120px] w-[120px] rounded-full overflow-hidden">
+                  <img src={i1} alt="image" className="rounded-full" />
+                </figure>
+                <h2 className="pt-4 text-2xl font-bold">Discover Skills</h2>
+                <h3 className="text-xl px-4 text-center text-blue-400 font-semibold">
+                  Explore your desired skills in various categories through
+                  internet
+                </h3>
               </div>
-            </div>
-            <div className="bg-base-200 shadow-sm flex flex-col items-center justify-center min-h-[300px] rounded-lg">
-              <figure className="h-[120px] w-[120px] rounded-full overflow-hidden">
-                <img src={i1} alt="image" className="rounded-full" />
-              </figure>
-              <h2 className="pt-4 text-2xl font-bold">Emran Hossen</h2>
-              <h3 className="text-xl text-blue-400 font-semibold">Spoken English Practice</h3>
-              <div className="flex gap-2 items-center py-2">
-                <p className="font-bold text-xl">Rating: </p>
-                <span>⭐⭐⭐⭐⭐</span>
+              <div className="bg-base-200 shadow-sm flex flex-col items-center justify-center min-h-[300px] rounded-lg">
+                <figure className="h-[120px] w-[120px] rounded-full overflow-hidden">
+                  <img src={i1} alt="image" className="rounded-full" />
+                </figure>
+                <h2 className="pt-4 text-2xl font-bold">Discover Skills</h2>
+                <h3 className="text-xl px-4 text-center text-blue-400 font-semibold">
+                  Explore your desired skills in various categories through
+                  internet
+                </h3>
               </div>
-            </div>
-            <div className="bg-base-200 shadow-sm flex flex-col items-center justify-center min-h-[300px] rounded-lg">
-              <figure className="h-[120px] w-[120px] rounded-full overflow-hidden">
-                <img src={i1} alt="image" className="rounded-full" />
-              </figure>
-              <h2 className="pt-4 text-2xl font-bold">Emran Hossen</h2>
-              <h3 className="text-xl text-blue-400 font-semibold">Spoken English Practice</h3>
-              <div className="flex gap-2 items-center py-2">
-                <p className="font-bold text-xl">Rating: </p>
-                <span>⭐⭐⭐⭐⭐</span>
+              <div className="bg-base-200 shadow-sm flex flex-col items-center justify-center min-h-[300px] rounded-lg">
+                <figure className="h-[120px] w-[120px] rounded-full overflow-hidden">
+                  <img src={i1} alt="image" className="rounded-full" />
+                </figure>
+                <h2 className="pt-4 text-2xl font-bold">Discover Skills</h2>
+                <h3 className="text-xl px-4 text-center text-blue-400 font-semibold">
+                  Explore your desired skills in various categories through
+                  internet
+                </h3>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     </>
